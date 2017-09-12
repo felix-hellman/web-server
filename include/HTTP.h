da02e76110731bd3927e684c55f084e489a3ccf6
Adam Georgsson <adamgeorgsson@gmail.com>
2017-09-13 00:02:37 +0200

    HTTP response

--- include/HTTP.h
+++ include/HTTP.h
@@ -2,6 +2,8 @@
 #define HTTP_H
 #include <stdlib.h>
 
+#define MAX_FSIZE 2000000
+#define MAX_HSIZE 300
 /*
  *	return codes
  *	0 = Reading done
@@ -10,5 +12,5 @@
 
 int GET(char * client_message, char * buffer, int buffersize, int offset);
 int HEAD(char * client_message, char * buffer, int buffersize, int offset);
-
+void createHeader(char *header);
 #endif
--- src/HTTP.c
+++ src/HTTP.c
@@ -1,23 +1,38 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 #include "HTTP.h"
 
-
-//Return status code
+//TODO read filename, check if size > buffersize
 int GET(char * client_message, char * buffer, int buffersize,int offset)
 {
-	buffer[0] = 'G';
-	buffer[1] = 'E';
-	buffer[2] = 'T';
+    char header[MAX_HSIZE];
+    createHeader(header);
+    char response[MAX_FSIZE + MAX_HSIZE];
+    strcpy(response, header);
 
-	return 0;
+    FILE *file = fopen("index.html", "r");
+    char ch;
+    int i = strlen(header);
+    while((ch = fgetc(file)) != EOF && i < MAX_FSIZE)
+	response[i++] = ch;
+    response[i] = '\0';
+    fclose(file);
+    
+    memcpy(buffer, &response[buffersize * offset], buffersize);
+
+    return 0;
 }
 
 int HEAD(char * client_message, char * buffer, int buffersize,int offset)
 {
-	buffer[0] = 'H';
-	buffer[1] = 'E';
-	buffer[2] = 'A';
-	buffer[3] = 'D';
+    createHeader(buffer);
+    
+    return 0;
+}
 
-	return 0;
+//TODO our own header
+void createHeader(char *header)
+{
+    strcpy(header, "HTTP/1.1 200 OK\nDate: Tue, 12 Sep 2017 19:49:32 GMT\nServer: Apache/2.4.10 (Raspbian)\nLast-Modified: Sun, 13 Nov 2016 21:54:02 GMT\nETag: \"c8-54135c2e00c0b\"\nAccept-Ranges: bytes\nContent-Length: 200\nVary: Accept-Encoding\nContent-Type: text/html\n");
 }
