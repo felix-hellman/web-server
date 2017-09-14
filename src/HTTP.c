e1b9e7ca4c656fef8c59a512870f0bbecb317dfd
Adam Georgsson <adamgeorgsson@gmail.com>
2017-09-14 23:20:57 +0200

    can handle wrong filename

--- src/HTTP.c
+++ src/HTTP.c
@@ -1,7 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <errno.h>
 #include "HTTP.h"
+
+extern int errno;
 
 int HTTP_Request(char *request, char **response, char *buffer, int buffersize, int offset)
 {
@@ -15,6 +18,7 @@ int HTTP_Request(char *request, char **response, char *buffer, int buffersize, i
 	return GET(request,buffer,buffersize,offset);
 }
 
+//TODO handle those statuscodes
 int GET(char *request, char *buffer, int buffersize, int offset)
 {
 	char filepath[PATH_SIZE];
@@ -43,12 +47,14 @@ int HEAD(char *request, char *buffer, int buffersize, int offset)
 
 int readFile(char *filepath, char *content)
 {
-	//TODO check permission, file existence
-	int statusCode;
 	FILE *file = fopen(filepath, "r");
 
 	if (file == NULL) {
-		statusCode = 500;
+		int errnum = errno;
+		if (strcmp(strerror(errnum), "No such file or directory") == 0)
+			return 404;
+		else
+			return 500;
 	}  else {
 		char ch;
 		int i = 0;
@@ -56,14 +62,13 @@ int readFile(char *filepath, char *content)
 			content[i++] = ch;
 		content[i] = '\0';
 		fclose(file);
-		statusCode = 200;
+		return 200;
 	}
-	return statusCode;
 }
 
+//TODO kolla om adressen ligger på tillåten plats. Annars blir det 403! realpath
 int extractFilename(char *request, char *filepath)
 {
-	int statusCode = 200;
 	int namelen = PATH_SIZE - strlen(WWW);
 	char *filename = calloc(namelen, sizeof(char));
 
@@ -73,17 +78,15 @@ int extractFilename(char *request, char *filepath)
 		filename[j++] = request[i++];
 	filename[j] = '\0';
 
-
 	if (filename[strlen(filename)-1] == '/')
 		strcat(filename, "index.html"); //possible overflow
-	if (request[i] == '\\' || request[i] == '\n' || request[i] == '\r')
-		statusCode = 501;
-
 	strcpy(filepath, WWW);
 	strcat(filepath, filename);
 	free(filename);
 
-	return statusCode;
+	if (request[i] == '\\' || request[i] == '\n' || request[i] == '\r')
+		return 501;
+	return 200;
 }
 
 //TODO correct date, error code
