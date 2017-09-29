62fd2d7eccc86df9b432bb213e5fb0e08dbd80f5
felix <felixhellman.ro@gmail.com>
2017-09-29 12:41:51 +0200

    Wrote interface for logging

--- include/log.h
+++ include/log.h
@@ -8,5 +8,17 @@
  * @brief functions that log requests to the server
  */
 
+/**
+ * @brief This function will be called from a thread to construct a log entry
+ */
+void writeToLog(char * filepath, HTTP_buffer * HTTP, int socketfd);
+/**
+ * @brief Writing to log file in a thread safe manner
+ */
+void writeToFile(char * filepath, char * buffer, int buffersize);
+/**
+ * @brief Fills the buffer with the clients IP-address
+ */
+void resolveIp(int socketfd, char * buffer);
 
 #endif
