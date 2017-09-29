bcc34249b71233b09286599a6885a615b6bd44f2
felix <felixhellman.ro@gmail.com>
2017-09-29 13:06:19 +0200

    Logging things

--- src/log.c
+++ src/log.c
@@ -2,5 +2,38 @@
 #include <stdio.h>
 #include <string.h>
 #include <limits.h>
+#include <fcntl.h>
 #include "log.h"
 
+void writeToLog(char * filepath, HTTP_buffer * HTTP, int socketfd)
+{
+	const int buffersize = 1024;
+	char * buffer = calloc(sizeof(char),buffersize);
+	char ip[20];
+	resolveIp(socketfd,ip);
+	/*TODO
+	 * FILL BUFFER HERE
+	 */
+
+	writeToFile(filepath,buffer,buffersize);
+	free(buffer);
+}
+void writeToFile(char * filepath, char * buffer, int buffersize)
+{
+	struct flock lock;
+	int fd = open(filepath, O_WRONLY | O_APPEND);
+
+	memset(&lock,0,sizeof(lock));
+	lock.l_type = F_WRLCK;
+	fctnl(fd,F_SETLKW,&lock); //Place lock
+	/*Write to file*/
+	write(fd,buffer,buffersize);
+	/*Exit critical section*/
+	lock.l_type = F_UNLCK;
+	fcntl(fd,F_SETLKW,&lock); //Unlock
+	close(fd);
+}
+void resolveIp(int fd, char * buffer)
+{
+	strlcpy(buffer,"127.0.0.1");
+}
